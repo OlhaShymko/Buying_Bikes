c34c850f3900dfa8376da755271b894909f9fcc7
Olha Shymko <olha.shymko.91@gmail.com>
2017-03-19 15:04:26 +0200

    класс Engine

---
+++ Buing_BikesEngine.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Администратор on 01.06.2016.
+ */
+public class Buing_BikesEngine implements ActionListener {
+
+    public void actionPerformed(ActionEvent event) {
+        JOptionPane.showConfirmDialog(null, "Order is accepted", "", JOptionPane.PLAIN_MESSAGE);
+
+        }
+
+    }
+
+
+
